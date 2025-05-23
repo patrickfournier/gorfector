0d2e37bbadac9399a22cd3d206ed63239feeed09
Patrick Fournier <patrick@patrickfournier.com>
2025-05-23 17:35:36 -0400

    build_appimage.sh now builds the app from scratch.

--- build-aux/appimage/build_appimage.sh
+++ build-aux/appimage/build_appimage.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 # This script builds the AppImage for the application.
-# Run it from the root of the repository.
-# meson setup must have been given the --prefix=/usr option.
+# Run it from the root of the repository:
+# ./build-aux/appimage/build_appimage.sh <build_dir> <app_dir>
+
+build_dir=$1
+app_dir=$2
 
 if [ ! -f "linuxdeploy-x86_64.AppImage" ]; then
     curl -L https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20250213-2/linuxdeploy-x86_64.AppImage -o linuxdeploy-x86_64.AppImage
     chmod +x linuxdeploy-x86_64.AppImage
 fi
 
-if [ -d "AppDir" ]; then
-    rm -rf "AppDir"
+if [ -d "${build_dir}" ]; then
+    rm -rf "${build_dir}"
 fi
 
-cd buildDir
-meson install --no-rebuild --destdir ../AppDir
+if [ -d "${app_dir}" ]; then
+    rm -rf "${app_dir}"
+fi
 
-cd ../AppDir
-rm usr/bin/gorfector-tests
+meson setup "${build_dir}" --prefix=/usr --buildtype release
+cd "${build_dir}" || exit 1
+meson install --destdir ../"${app_dir}"
+
+cd ../"${app_dir}" || exit
+rm -f usr/bin/gorfector-tests
 mv usr/share/metainfo/com.patrickfournier.gorfector.metainfo.xml usr/share/metainfo/com.patrickfournier.gorfector.appdata.xml
 
 cd ..
-./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
-
+./linuxdeploy-x86_64.AppImage --appdir "${app_dir}" --output appimage
