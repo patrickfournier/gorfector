ebdbecb9c15c24b2dc2d7029a1caddb3358b39fc
Patrick Fournier <patrick@patrickfournier.com>
2025-05-24 11:53:33 -0400

    Add missing translation

--- src/ZooLib/ErrorDialog.cpp
+++ src/ZooLib/ErrorDialog.cpp
@@ -1,12 +1,13 @@
-#include "ErrorDialog.hpp"
-
 #include <string>
+
+#include "ErrorDialog.hpp"
+#include "Gettext.hpp"
 
 namespace ZooLib
 {
     void ShowUserError(AdwApplicationWindow *parentWindow, const std::string &message)
     {
-        auto dialog = adw_alert_dialog_new("Error", nullptr);
+        auto dialog = adw_alert_dialog_new(_("Error"), nullptr);
         adw_alert_dialog_set_body(ADW_ALERT_DIALOG(dialog), message.c_str());
         adw_alert_dialog_add_response(ADW_ALERT_DIALOG(dialog), "ok", "OK");
         adw_alert_dialog_set_default_response(ADW_ALERT_DIALOG(dialog), "ok");
